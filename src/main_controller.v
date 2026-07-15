676fe7ef64056f21049d87fe38293321ae2a11df
stormloop <52413267+stormloop@users.noreply.github.com>
2026-07-15 13:46:27 +0000

    main_controller, timer headers and controller pmod

---
+++ src/main_controller.v
@@ -0,0 +1,25 @@
+/*
+ * The main controller of the tamagotchi manages input signals, battery levels, etc., and connects with other modules like the minigame and VGA.
+ * Features:
+ *  - resetting the controller: the cat respawns with a bang, with full battery and 9 lives, in the default state.
+ *  - left, right, up and down move the cat.
+ *  - X, Y, A and B for going to 'play', 'sleep', 'eat' and 'default' state respectively
+ *      Note: can the cat go from one state directly to the next? does it always have to go to default first? does it matter?
+ *  - If the controller gets a deplete_battery signal from the timer, it should deplete its battery levels by one, if the battery reaches zero, a heart is lost.
+ *  - If all hearts are gone, the cat dies. It respawns with a bang.
+ *  - If a fish is caught, the cat plays or the cat sleeps, the battery increases again.
+ */
+module main_controller (
+    input reset, clk,                                       // Global active-low reset and clock.
+    input left, right, up, down, A, B, X, Y,                // Inputs from the Controller pmod.
+    input deplete_battery,                                  // Signals that the battery has to drop one level.
+    input fish_caught,                                      // Signals that a fish has been caught.
+    output [9:0] cat_pos_x,                                 // The x-position of the cat.
+    output [8:0] cat_pos_y,                                 // The y-position of the cat.
+    output [3:0] lives_left,                                // The number of lives the cat has left, to be shown on the VGA.
+    output [3:0] battery_left,                              // The number of battery bars the cat has left, to be shown on the VGA.
+    output is_eating,                                       // Signals that the food minigame is currently active.
+    output show_bang, is_dead, is_sleeping, is_playing      // Signals to the VGA about extra stuff to render and to the timer to affect the battery.
+);
+
+endmodule
---
+++ src/timer.v
@@ -0,0 +1,17 @@
+/*
+ * The timer takes care of timing-related events.
+ * Three internal clocks are kept to keep track of the how long it's been since the cat slept, ate or played.
+ * If the cat sleeps, eats or plays, that respective timer is reset.
+ * If a timer runs out, a deplete_battery pulse is sent, and that timer is reset.
+ *      Suggestion: if a timer runs out, reset it to a lower value, so it runs out again quicker.
+ * The active-low reset resets all timers.
+ * If the cat loses a heart or respawns, all timers reset as well. This is signaled through 'reset' as well.
+ * Note: the timers could either count up or down, maybe counting down is easier as then each timer has the same 'has depleted' check: timer == 0
+ */
+module timer (
+    input reset, clk,                                               // Global active-low reset and clock.
+    input is_sleeping, caught_fish, is_playing,                     // Signals to reset/increase the timers/battery.
+    output deplete_battery                                          // Signals that the battery has to drop one level.
+);
+
+endmodule
