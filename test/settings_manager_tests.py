5e0e04ac7b8e1a2a3127c67cc69b45b1d5e47610
stormloop <52413267+stormloop@users.noreply.github.com>
2026-07-20 16:02:39 +0000

    implemented module

--- test/settings_manager_tests.py
+++ test/settings_manager_tests.py
@@ -29,13 +29,11 @@ async def reset_resets_settings_test(dut):
     dut._log.info("Changing settings.")
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 1)
-    dut.inputs_a.value[0] = 1
-    dut.inputs_a.value[2] = 1
-    dut.inputs_b.value[1] = 1
+    dut.inputs_a.value = (1 << 2) + (0 << 1) + (1 << 0)
+    dut.inputs_b.value = (1 << 1) + (0 << 0)
     await ClockCycles(dut.clk, 1)
-    dut.inputs_a.value[0] = 0
-    dut.inputs_a.value[2] = 0
-    dut.inputs_b.value[1] = 0
+    dut.inputs_a.value = 0
+    dut.inputs_b.value = 0
     await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 1
     await ClockCycles(dut.clk, 1)
@@ -47,7 +45,7 @@ async def reset_resets_settings_test(dut):
     dut.rst_n.value = 1
     await ClockCycles(dut.clk, 1)
     assert all([int(dut.outputs_a.value[i]) == 0 for i in range(3)])
-    assert all([int(dut.outputs_b.value[i*2-1:(i-1)*2]) == 0 for i in range(2)])
+    assert all([int(dut.outputs_b.value[(i+1)*2-1:i*2]) == 0 for i in range(2)])
     dut._log.info("    Reset successful.")
 
 
@@ -56,16 +54,14 @@ async def settings_do_not_change_with_reset_high_test(dut):
     await setup_test(dut)
 
     dut._log.info("Changing settings while rst_n=1.")
-    dut.inputs_a.value[0] = 1
-    dut.inputs_a.value[2] = 1
-    dut.inputs_b.value[1] = 1
+    dut.inputs_a.value = (1 << 2) + (0 << 1) + (1 << 0)
+    dut.inputs_b.value = (1 << 1) + (0 << 0)
     await ClockCycles(dut.clk, 1)
-    dut.inputs_a.value[0] = 0
-    dut.inputs_a.value[2] = 0
-    dut.inputs_b.value[1] = 0
+    dut.inputs_a.value = 0
+    dut.inputs_b.value = 0
     await ClockCycles(dut.clk, 1)
     assert all([dut.outputs_a.value[i] == 0 for i in range(3)])
-    assert all([dut.outputs_b.value[i*2-1:(i-1)*2] == 0 for i in range(2)])
+    assert all([dut.outputs_b.value[(i+1)*2-1:i*2] == 0 for i in range(2)])
     dut._log.info("    Success: settings did not change.")
 
 
@@ -75,35 +71,25 @@ async def change_settings_test(dut):
 
     dut._log.info("Changing settings.")
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 1)
-    dut.inputs_a.value[0] = 1
-    dut.inputs_a.value[2] = 1
-    dut.inputs_b.value[1] = 1
+    await ClockCycles(dut.clk, 3)
+    dut.inputs_a.value = (1 << 2) + (0 << 1) + (1 << 0)
+    dut.inputs_b.value = (1 << 1) + (0 << 0)
     await ClockCycles(dut.clk, 10)
-    dut.inputs_a.value[0] = 0
-    dut.inputs_a.value[2] = 0
-    dut.inputs_b.value[1] = 0
+    dut.inputs_a.value = 0
+    dut.inputs_b.value = 0
     await ClockCycles(dut.clk, 1)
-    dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 1)
-    assert dut.outputs_a.value[0] == 0
+    assert dut.outputs_a.value[0] == 1
     assert dut.outputs_a.value[1] == 0
     assert dut.outputs_a.value[2] == 1
     assert int(dut.outputs_b.value[1:0]) == 0
     assert int(dut.outputs_b.value[3:2]) == 1
     dut._log.info("    Settings changed successfully (first time).")
 
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 1)
-    dut.inputs_a.value[0] = 1
-    dut.inputs_a.value[2] = 1
-    dut.inputs_b.value[1] = 1
+    dut.inputs_a.value = (1 << 2) + (0 << 1) + (1 << 0)
+    dut.inputs_b.value = (1 << 1) + (0 << 0)
     await ClockCycles(dut.clk, 10)
-    dut.inputs_a.value[0] = 0
-    dut.inputs_a.value[2] = 0
-    dut.inputs_b.value[1] = 0
-    await ClockCycles(dut.clk, 1)
-    dut.rst_n.value = 1
+    dut.inputs_a.value = 0
+    dut.inputs_b.value = 0
     await ClockCycles(dut.clk, 1)
     assert dut.outputs_a.value[0] == 0
     assert dut.outputs_a.value[1] == 0
@@ -112,17 +98,11 @@ async def change_settings_test(dut):
     assert int(dut.outputs_b.value[3:2]) == 2
     dut._log.info("    Settings changed successfully (second time).")
 
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 1)
-    dut.inputs_a.value[0] = 1
-    dut.inputs_a.value[2] = 1
-    dut.inputs_b.value[1] = 1
+    dut.inputs_a.value = (1 << 2) + (0 << 1) + (1 << 0)
+    dut.inputs_b.value = (1 << 1) + (0 << 0)
     await ClockCycles(dut.clk, 10)
-    dut.inputs_a.value[0] = 0
-    dut.inputs_a.value[2] = 0
-    dut.inputs_b.value[1] = 0
-    await ClockCycles(dut.clk, 1)
-    dut.rst_n.value = 1
+    dut.inputs_a.value = 0
+    dut.inputs_b.value = 0
     await ClockCycles(dut.clk, 1)
     assert dut.outputs_a.value[0] == 1
     assert dut.outputs_a.value[1] == 0
@@ -131,15 +111,11 @@ async def change_settings_test(dut):
     assert int(dut.outputs_b.value[3:2]) == 3
     dut._log.info("    Settings changed successfully (third time).")
 
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 1)
-    dut.inputs_a.value[0] = 1
-    dut.inputs_a.value[2] = 1
-    dut.inputs_b.value[1] = 1
+    dut.inputs_a.value = (1 << 2) + (0 << 1) + (1 << 0)
+    dut.inputs_b.value = (1 << 1) + (0 << 0)
     await ClockCycles(dut.clk, 10)
-    dut.inputs_a.value[0] = 0
-    dut.inputs_a.value[2] = 0
-    dut.inputs_b.value[1] = 0
+    dut.inputs_a.value = 0
+    dut.inputs_b.value = 0
     await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 1
     await ClockCycles(dut.clk, 1)
