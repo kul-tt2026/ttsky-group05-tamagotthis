0f133e15a4a2fd096ca68df63eca2e84f502318a
majavanhoof <maja.vanhoof@student.kuleuven.be>
2026-07-29 14:06:03 +0000

    added 2nd clock to minigame

--- src/minigame.v
+++ src/minigame.v
@@ -3,7 +3,7 @@
  * It determines the position of the fish and checks if the cat has caught it.
  */
 module minigame (
-    input rst_n, clk,               // Global active-low reset and clock.
+    input rst_n, clk, clk2,         // Global active-low reset and clock  + faster clock for lfsr (clk2)
     input is_eating,                // Signals that the food minigame is currently active.
     input [9:0] cat_pos_x,          // The x-position of the cat.
     input [9:0] cat_pos_y,          // The y-position of the cat. 
